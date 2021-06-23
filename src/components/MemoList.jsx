f3272f1f9856ee68b97c651940efab8fb570e630
Sliver <yosi_4410u@outlook.jp>
2021-06-23 18:47:54 +0900

    Minor fixes

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-
+    flex: 1,
   },
   memoTitle: {
     fontSize: 16,
